d5be1ecd7649c1fdb9003726feceffe1a399026a
TrungCorp <trungle9995@gmail.com>
2024-10-03 03:37:55 -0400

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    k = 10
+    while k>0:
+        print(k)
+        k -=1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+     list_obj = [num*num for num in int_list]
+     return list_obj
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    k = 1
+    while(k<=100):
+        if k % 3 == 0 and k % 5 == 0:
+            print("FizzBuzz")
+            k +=1
+        elif k % 3 == 0:
+            print("Fizz")
+            k +=1
+        elif k % 5 == 0:
+            print("Buzz")
+            k +=1
+        else:
+            print(k)
+            k +=1
+            
